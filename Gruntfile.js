530dc66502b90e9150402f4a406f2a436f1797d6
Andrew Nichols <tandrewnichols@gmail.com>
2014-09-05 20:32:56 -0400

    Make gruntfile require package.main

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,10 @@
-var tm = require('./lib/task-master');
+// Yes, I could just require './lib/task-master', but I've
+// previously accidentally published this with "main" pointing
+// to the wrong thing, so this is a safeguard against that. It's
+// similar to what you get when you install task-master and
+// require it.
+var pkg = require('./package');
+var tm = require(pkg.main);
 
 module.exports = function(grunt) {
   tm(grunt); 
